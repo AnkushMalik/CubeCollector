71da3ee927037738a5acd044e39ee2f4c85bc535
AnkushMalik <ankushmalik631@gmail.com>
2018-06-14 13:40:38 +0530

    Add forces

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+
+	public float speed = 5;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+	void Update() {  // place where our instructions before rendering a frame, are placed
+	}
+	void FixedUpdate() {  // place where our physics calculations are placed
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3( moveHorizontal, 0.0f, moveVertical );
+
+		rb.AddForce(movement * speed);
+	}
+}
