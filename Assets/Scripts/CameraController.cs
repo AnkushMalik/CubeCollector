43ae008819ac9811d57b256cac9408ca05bcdbf1
AnkushMalik <ankushmalik631@gmail.com>
2018-06-14 15:46:37 +0530

    Add CameraController
    Add relation between Player and Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	// LateUpdate is called once per frame after processing all instructions from update
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
