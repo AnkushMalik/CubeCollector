d567ca1977663265776116da0c9c3d4b591ad7c5
AnkushMalik <ankushmalik631@gmail.com>
2018-06-18 15:33:37 +0530

    Add text to game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
+	private int count;
 
 	public float speed = 5;
+	public Text countText;
 
 	void Start(){
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		countText.text = "Score : " + count.ToString();
 	}
 
 	void Update() {  // place where our instructions before rendering a frame, are placed
@@ -28,6 +33,8 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag("Collectibles"))
 		{
 			other.gameObject.SetActive (false);
+			count += 1;
+			countText.text = "Score : " + count.ToString();
 		}
 	}
 }
